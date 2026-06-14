f1dfcdf8fb7d5a6eaa55452bc4121d70bf3a7a1e
yb9255 <yoobin9255@gmail.com>
2026-06-14 23:38:57 +0900

    Magnitude + transform 기반 particle disperse 예시 추가
    Signed-off-by: yb9255 <yoobin9255@gmail.com>

--- particle/index.js
+++ particle/index.js
@@ -3,6 +3,7 @@ import range from 'lodash.range';
 
 const btn = document.querySelector('.particleButton');
 const FADE_DURATION = 1000;
+const MAGNITUDE = 48;
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('liked');
@@ -15,12 +16,28 @@ btn.addEventListener('click', () => {
 
   const particles = [];
 
+  // top - left 포지셔닝 기반 좌표 업데이트
   range(5).forEach(() => {
     const particle = document.createElement('span');
     particle.classList.add('particle');
 
     particle.style.top = random(0, 100) + '%';
     particle.style.left = random(0, 100) + '%';
+    particle.style.setProperty('--fade-duration', FADE_DURATION + 'ms');
+
+    btn.appendChild(particle);
+    particles.push(particle);
+  });
+
+  // Magnitude 기반, 가운데 -> 사방으로 펼쳐지는 좌표 업데이트
+  range(5).forEach(() => {
+    const particle = document.createElement('span');
+    particle.classList.add('particle2');
+
+    const x = random(-MAGNITUDE, MAGNITUDE);
+    const y = random(-MAGNITUDE, MAGNITUDE);
+    particle.style.transform = `translate(${x}px, ${y}px)`;
+
     particle.style.setProperty('--fade-duration', FADE_DURATION + 'ms');
 
     btn.appendChild(particle);
